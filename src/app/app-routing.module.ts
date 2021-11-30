abf97ca857cb9d37371b2682b315d5be2b69e1b0
Tara Z. Manicsic <tzmanics@gmail.com>
2021-11-29 22:43:41 -0500

    adds account page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
