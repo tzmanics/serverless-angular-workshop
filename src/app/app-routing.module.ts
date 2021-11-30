a943dcae856edadabc6c7cbf7f15b49abdc06f5c
Tara Z. Manicsic <tzmanics@gmail.com>
2021-11-29 23:37:56 -0500

    adds home page and styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule) }, { path: 'donate', loadChildren: () => import('./donate/donate.module').then(m => m.DonateModule) }];
+const routes: Routes = [{ path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule) }, { path: 'donate', loadChildren: () => import('./donate/donate.module').then(m => m.DonateModule) }, { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
