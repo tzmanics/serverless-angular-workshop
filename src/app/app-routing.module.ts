022579ba08c3dfe8d32a22b791d0cff5730dbd9b
Tara Z. Manicsic <tzmanics@gmail.com>
2021-11-29 23:39:57 -0500

    moves pages into pages dir

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule) }, { path: 'donate', loadChildren: () => import('./donate/donate.module').then(m => m.DonateModule) }, { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }];
+const routes: Routes = [
+  {
+    path: 'account',
+    loadChildren: () =>
+      import('./pages/account/account.module').then((m) => m.AccountModule),
+  },
+  {
+    path: 'donate',
+    loadChildren: () =>
+      import('./pages/donate/donate.module').then((m) => m.DonateModule),
+  },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomeModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
