7f00a41c65e68e2b13fd022dec061c7f1dd4d232
Tara Z. Manicsic <tzmanics@gmail.com>
2021-12-02 11:05:04 -0600

    updates package.json and org item

---
+++ netlify/functions/createCheckout.js
@@ -0,0 +1,30 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async (event) => {
+  const organization = JSON.parse(event.body);
+  const lineItems = [
+    {
+      name: organization.name,
+      currency: "USD",
+      description: organization.description,
+      images: [organization.image],
+      amount: `${organization.donationAmount}00`,
+      quantity: 1,
+    },
+  ];
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    success_url: process.env.URL,
+    cancel_url: `${process.env.URL}/donate`,
+    line_items: lineItems,
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      sessionId: session.id,
+      publishableKey: `${process.env.PUBLISHABLE_KEY}`,
+    }),
+  };
+};
--- src/app/components/organization-item/organization-item.component.ts
+++ src/app/components/organization-item/organization-item.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { loadStripe } from '@stripe/stripe-js';
+import { lastValueFrom } from 'rxjs';
 
 import { Organization } from 'src/app/models/Organization';
 
@@ -17,7 +20,28 @@ export class OrganizationItemComponent implements OnInit {
     image: 'https://bit.ly/2ZdePYO',
   };
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  triggerCreateCheckout = async (eventOrganization: any) => {
+    const createCheckoutResponse = this.http.post(
+      '/.netlify/functions/createCheckout',
+      eventOrganization,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    const lastResponse = await lastValueFrom(createCheckoutResponse);
+    this.openStripe(lastResponse);
+  };
+
+  openStripe = async (stripeParams: any) => {
+    const stripe = await loadStripe(stripeParams.publishableKey);
+    const { error } = await stripe!.redirectToCheckout({
+      sessionId: stripeParams.sessionId,
+    });
+  };
 
   ngOnInit(): void {}
 }
