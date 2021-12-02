cf51c980a8e2de80cff113e8156adf01dd55a4a3
Tara Z. Manicsic <tzmanics@gmail.com>
2021-12-02 04:14:30 -0600

    ./

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,10 +1,12 @@
 exports.handler = async (event, context) => {
+  const name = event.queryStringParameters.name || "Buddy";
+
   console.log("Hello Angular World!");
   console.log(`\nHere is the event info: ${JSON.stringify(event)}`);
   console.log(`\nHere is the context info: ${JSON.stringify(context)}`);
 
   return {
     statusCode: 200,
-    body: "This function is totes working, Yay!",
+    body: `This function is totes working, Yay!\nHello, ${name}!`,
   };
 };
